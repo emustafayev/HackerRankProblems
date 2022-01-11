da774b65091c21fff8761c0878a0de16fbdceb4a
amustafayev <afgan.mustafayev@pashabank.az>
2022-01-12 01:58:54 +0400

    Added container with the most water problem solution

---
+++ src/main/java/leet_code/ContainerWithMostWater.java
@@ -0,0 +1,27 @@
+package leet_code;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+
+
+    public static int maxArea(int[] height) {
+        int leftP = 0;
+        int rightP = height.length - 1;
+
+        int maxArea = 0;
+
+        while (leftP < rightP){
+
+            maxArea = Math.max(maxArea,(rightP-leftP) * Math.min(height[rightP],height[leftP]));
+
+            if (height[leftP] > height[rightP]){ // if left height is greater than right, then there should be chance of
+                rightP--;                        // bigger right height in order to get bigger area
+            }else {         // and vice-versa
+                leftP++;
+            }
+        }
+        return maxArea;
+    }
+}
