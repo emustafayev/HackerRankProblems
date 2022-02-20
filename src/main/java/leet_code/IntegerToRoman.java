d51de0e7bfa752ad82aad1f08dbc99da3d8b37f6
amustafayev <afgan.mustafayev@pashabank.az>
2022-02-20 15:43:14 +0400

    IntegerToRoman solution added

--- src/main/java/leet_code/IntegerToRoman.java
+++ src/main/java/leet_code/IntegerToRoman.java
@@ -7,17 +7,27 @@ public class IntegerToRoman {
         System.out.println(intToRoman(num));
     }
 
+
     public static String intToRoman(int num) {
-        while (num > 0) {
 
+        String s = "";
 
-            int last = num % 10;
+        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-            System.out.println(last);
-
-            num = num / 10;
+        for (int i = 0; i < nums.length; i++) {  //347   // CCCXLVII
+            // Go until we can divide;
+            if (num / nums[i] > 0) { // 1. 347 / 100
+                // How many times that digit can be divided?
+                int romanDigit = num / nums[i];
+                // Add repititions
+                for (int j = 0; j < romanDigit; j++) {
+                    s = s.concat(romans[i]);
+                }
+                // Pass to next number
+                num = num % nums[i];
+            }
         }
-
-        return null;
+        return s;
     }
 }
