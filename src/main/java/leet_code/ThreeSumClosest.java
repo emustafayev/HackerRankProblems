c2a12040274ef180c262dcdb555723cefe00611a
amustafayev <afgan.mustafayev@pashabank.az>
2022-04-07 18:48:05 +0400

    Three sum closest solution added from leetcode

---
+++ src/main/java/leet_code/ThreeSumClosest.java
@@ -0,0 +1,42 @@
+package leet_code;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+    public static void main(String[] args) {
+        System.out.println(threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+    }
+
+    private static int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int rL = -10000;
+        int rU = 10000;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int l = i + 1;
+            int h = nums.length - 1;
+            while (l < h) {
+                int sum = nums[i] + nums[l] + nums[h];
+                if (sum == target) {
+                    return sum;
+                } else if (sum > target) {
+                    rU = Math.min(rU, sum);
+                    h--;
+                } else {
+                    rL = Math.max(rL, sum);
+                    l++;
+                }
+            }
+        }
+
+        if (Math.abs(target - rU) < Math.abs(target - rL)) {
+            return rU;
+        } else {
+            return rL;
+        }
+    }
+
+}
