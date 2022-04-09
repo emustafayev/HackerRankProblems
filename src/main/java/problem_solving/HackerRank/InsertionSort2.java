3f104af8a7f89c5c551a55e7174b02e63632cd32
amustafayev <afgan.mustafayev@pashabank.az>
2022-04-10 02:56:41 +0400

    Insertion sort added

---
+++ src/main/java/problem_solving/HackerRank/InsertionSort2.java
@@ -0,0 +1,40 @@
+package problem_solving.HackerRank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InsertionSort2 {
+
+    public static void main(String[] args) {
+        insertionSortPart2(6, Arrays.asList(1, 4, 3, 5, 6, 2));
+    }
+
+
+    public static void insertionSortPart2(int n, List<Integer> arr) {
+
+        int[] array = arr.stream()
+                .mapToInt(x -> x)
+                .toArray();
+
+        for (int i = 1; i < n; i++) {
+//          1, 4, 3, 5, 6, 2
+
+            int j = i;
+            int currEl = array[i]; // We need to put this value to its place
+
+            while (j >= 1 && currEl < array[j - 1]) { // If my element is smaller, swap it to left by swapting other elements
+                array[j] = array[j - 1];  // swap previous and curr element
+                j--; // go back to see if there is bigger then current element
+            }
+            array[j] = currEl; // j decrement last time end of the loop and point to index of currElement
+            printArray(array);
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
