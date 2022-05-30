65469cc0868d68280e31c6f2f4465c2000caa531
amustafayev <afgan.mustafayev@pashabank.az>
2022-05-31 02:07:13 +0400

    Some solutions added

---
+++ src/main/java/leet_code/GenerateParentheses.java
@@ -0,0 +1,19 @@
+package leet_code;
+
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static List<String> generateParenthesis(int n) {
+
+        
+
+        throw new IllegalArgumentException("Not impl yet");
+    }
+}
--- src/main/java/leet_code/MergeTwoSortedList.java
+++ src/main/java/leet_code/MergeTwoSortedList.java
@@ -41,7 +41,36 @@ public class MergeTwoSortedList {
         if (list1.val < list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
             return list1;
-        } else list2.next = mergeTwoLists(list2.next, list1);
+        } else list2.next = mergeTwoLists(list1, list2.next);
         return list2;
+
     }
+
+
+    public static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+
+        ListNode curr = head;
+
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        //remaining
+
+        if (list1 != null) curr.next = list1;
+        if (list2 != null) curr.next = list2;
+
+        return head.next;
+    }
+
 }
