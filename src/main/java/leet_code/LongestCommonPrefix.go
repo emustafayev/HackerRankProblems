fa9689f28ee0401e595f1d9fcd5879b6ea820577
amustafayev <afgan.mustafayev@pashabank.az>
2023-02-02 10:34:11 +0400

    Add new solutions from leetcode

---
+++ src/main/java/leet_code/LongestCommonPrefix.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+}
+
+func longestCommonPrefix(strs []string) string {
+
+	//for i := 0; i < len(strs); i++ {
+	//	var pref = strs[i][0]
+	//
+	//	for str := range strs[1:] {
+	//
+	//	}
+	//
+	//}
+
+	return ""
+}
---
+++ src/main/java/leet_code/SnakesAndLadders.java
@@ -0,0 +1,25 @@
+package leet_code;
+
+public class SnakesAndLadders {
+
+
+    public static void main(String[] args) {
+        snakesAndLadders(new int[][]{{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1,-1},{-1,35,-1,-1,13,-1},{-1,-1,-1,-1,-1,-1},{-1,15,-1,-1,-1,-1}});
+    }
+
+
+    //[[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
+
+
+    public static int snakesAndLadders(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.printf("%d ",board[i][j]);
+            }System.out.println();
+        }
+
+
+
+        throw new IllegalArgumentException("Not impl yet");
+    }
+}
