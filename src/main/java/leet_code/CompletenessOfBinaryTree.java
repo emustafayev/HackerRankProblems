868d6af91e3d44df1fd1b9d514b85f7cc859fae1
amustafayev <afgan.mustafayev@pashabank.az>
2023-03-15 16:10:38 +0400

    Add solutions from leetcode

---
+++ src/main/java/leet_code/CompletenessOfBinaryTree.java
@@ -0,0 +1,58 @@
+package leet_code;
+
+import com.sun.source.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CompletenessOfBinaryTree {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static boolean isCompleteTree(TreeNode root) {
+
+        Queue<TreeNode> qu = new LinkedList<TreeNode>();
+
+        qu.offer(root);
+
+        while (qu.peek() != null) {
+            TreeNode curr = qu.poll();
+            qu.offer(curr.left);
+            qu.offer(curr.right);
+            System.out.println(qu);
+        }
+
+        System.out.println(qu);
+
+        while (!qu.isEmpty() && qu.peek() == null) {
+            qu.remove();
+        }
+
+        return qu.isEmpty();
+
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
